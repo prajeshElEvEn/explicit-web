95914261fb85152894b3d515d67fc171dcf79451
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-05-21 15:31:48 +0530

    - added toast

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './styles/App.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import HomePage from './pages/HomePage';
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
+      <ToastContainer />
     </>
   );
 }
